923ee4ac5d06f74c2a2c10f1ff0c5d6f6a29176a
junzhi <junzhiwa@usc.edu>
2018-02-09 12:49:55 -0800

    Pytorch layer init

--- assignment2/conv_test.py
+++ assignment2/conv_test.py
@@ -1,34 +1,41 @@
-from layers import *
-from cs231n.fast_layers import max_pool_forward_fast, max_pool_backward_fast
-from time import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
 
-def rel_error(x, y):
-  """ returns relative error """
-  return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+import torchvision.datasets as dset
+import torchvision.transforms as T
 
-np.random.seed(231)
-x = np.random.randn(100, 3, 500, 500)
-dout = np.random.randn(100, 3, 250, 250)
-pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
+import numpy as np
 
-t0 = time()
-out_naive, cache_naive = max_pool_forward_fast(x, pool_param)
-t1 = time()
-out_fast, cache_fast = max_pool_forward_general(x, pool_param)
-t2 = time()
+import timeit
 
-print('Testing pool_forward_fast:')
-print('Naive: %fs' % (t1 - t0))
-print('fast: %fs' % (t2 - t1))
-print('speedup: %fx' % ((t1 - t0) / (t2 - t1)))
+dtype = torch.FloatTensor # the CPU datatype
 
-t0 = time()
-dx_naive = max_pool_backward_fast(dout, cache_naive)
-t1 = time()
-dx_fast = max_pool_backward_general(dout, cache_fast)
-t2 = time()
+class Flatten(nn.Module):
+	def forward(self, x):
+		N, C, H, W = x.size()  # read in N, C, H, W
+		return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
 
-print('\nTesting pool_backward_fast:')
-print('Naive: %fs' % (t1 - t0))
-print('fast: %fs' % (t2 - t1))
-print('speedup: %fx' % ((t1 - t0) / (t2 - t1)))
+
+in_channel, out_channel, kernel_size, stride = 3, 32, 7, 1
+pool_size, pool_stride = 2, 2
+affine_in, affine_out = 5408, 1024
+affine_in_2, affine_out_2 = 1024, 10
+
+fixed_model_base = nn.Sequential(
+	nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride),
+	nn.ReLU(),
+	nn.BatchNorm2d(out_channel, affine=True),
+	nn.MaxPool2d(kernel_size=pool_size),
+	Flatten(),
+	nn.Linear(affine_in, affine_out),
+	nn.ReLU(),
+	nn.Linear(affine_in_2, affine_out_2)
+)
+
+fixed_model_base.type(dtype)
+loss_fn = nn.CrossEntropyLoss().type(dtype)
+optimizer = optim.RMSprop(fixed_model_base.parameters())
